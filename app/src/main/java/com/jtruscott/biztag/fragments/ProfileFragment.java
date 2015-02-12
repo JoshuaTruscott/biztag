485cd4ef6bc8e68a24f3e04c4318c7254183fbc8
Joshua Truscott <joshtruscott95@gmail.com>
2015-02-12 22:01:26 +1100

    ListView Fixed

--- app/src/main/java/com/jtruscott/biztag/fragments/ProfileFragment.java
+++ app/src/main/java/com/jtruscott/biztag/fragments/ProfileFragment.java
@@ -1,7 +1,5 @@
 package com.jtruscott.biztag.fragments;
 
-import android.app.ActionBar;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -9,21 +7,22 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ActionMenuView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.jtruscott.biztag.R;
 import com.jtruscott.biztag.library.DatabaseHandler;
-import com.jtruscott.biztag.library.UserFunctions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static android.view.View.OnFocusChangeListener;
+import static com.jtruscott.biztag.R.id.editText;
 import static com.jtruscott.biztag.R.id.rowLayout;
 
 
@@ -45,27 +44,6 @@ public class ProfileFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
 
         mListView = (ListView) v.findViewById(R.id.profileListView);
-//            });
-        profileList = new ArrayList<String>();
-        profileList.add("My first list item");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
-        profileList.add("More items");
 
         mListView.setAdapter(new ProfileAdapter());
 
@@ -79,7 +57,7 @@ public class ProfileFragment extends Fragment {
 
         @Override
         public int getCount() {
-            return profileList.size();
+            return 6;
         }
 
         @Override
@@ -96,19 +74,52 @@ public class ProfileFragment extends Fragment {
         public View getView(int position, View convertView, ViewGroup parent) {
 
             View rowView = getActivity().getLayoutInflater().inflate(R.layout.row,null);
-            LinearLayout linearLayout = (LinearLayout) rowView.findViewById(R.id.rowLayout);
-            ImageView imageView = new ImageView(getActivity().getApplicationContext());
-            imageView.setImageResource(R.drawable.facebookicon);
-            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
-            imageView.setLayoutParams(layoutParams);
-            linearLayout.addView(imageView);
-            return linearLayout;
+
+            switch (position){
+                case 0:
+                    drawStringRow(rowView,"Full Name");
+                    break;
+                case 1:
+                    drawStringRow(rowView,"Contact Number");
+                    break;
+                case 2:
+                    drawStringRow(rowView,"Email Address");
+                    break;
+                case 3:
+                    drawImageRow(rowView,R.drawable.facebookicon);
+                    break;
+                case 4:
+                    drawImageRow(rowView,R.drawable.twittericon);
+                    break;
+                case 5:
+                    drawImageRow(rowView,R.drawable.instagramicon);
+                    break;
+
+            }
+            return rowView;
         }
     }
 
-    private View drawRow(View v, int id){
-
-
+    private void drawImageRow(View v, int id) {
+        LinearLayout linearLayout = (LinearLayout) v.findViewById(R.id.rowLayout);
+        ImageView imageView = new ImageView(getActivity().getApplicationContext());
+        imageView.setImageResource(id);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+        layoutParams.topMargin = 10;
+        imageView.setLayoutParams(layoutParams);
+        linearLayout.addView(imageView,0);
+    }
+    private void drawStringRow(View v, String s){
+        LinearLayout linearLayout = (LinearLayout) v.findViewById(R.id.rowLayout);
+        TextView textView = new TextView(getActivity().getApplicationContext());
+        textView.setText(s);
+        textView.setTextColor(getResources().getColor(R.color.black));
+        textView.setTextSize(20);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+        layoutParams.topMargin = 10;
+        textView.setLayoutParams(layoutParams);
+        linearLayout.addView(textView,0);
     }
 }
