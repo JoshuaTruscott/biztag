c3f0b7311e385adb9cebfb0025eac772ca70668f
Joshua Truscott <joshtruscott95@gmail.com>
2015-02-12 18:25:38 +1100

    Row adjustment

--- app/src/main/java/com/jtruscott/biztag/fragments/ProfileFragment.java
+++ app/src/main/java/com/jtruscott/biztag/fragments/ProfileFragment.java
@@ -1,21 +1,39 @@
 package com.jtruscott.biztag.fragments;
 
+import android.app.ActionBar;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ActionMenuView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.jtruscott.biztag.R;
+import com.jtruscott.biztag.library.DatabaseHandler;
 import com.jtruscott.biztag.library.UserFunctions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static com.jtruscott.biztag.R.id.rowLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private ListView mListView;
+    private List<String> profileList;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -25,20 +43,72 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_profile, container, false);
-//            Button mButton = (Button) v.findViewById(R.id.registerLinkButton);
-//            mButton.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Log.i("dBug","onClick() called :)");
-//                }
+
+        mListView = (ListView) v.findViewById(R.id.profileListView);
 //            });
+        profileList = new ArrayList<String>();
+        profileList.add("My first list item");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+        profileList.add("More items");
+
+        mListView.setAdapter(new ProfileAdapter());
+
+        DatabaseHandler dbh = new DatabaseHandler(getActivity().getApplicationContext());
+        HashMap<String,String> userInfo = dbh.getUserDetails();
+        Log.i("DBH",userInfo.toString());
             return v;
 
     }
-//    public void logoutClicked(View v){
-//        new UserFunctions().logoutUser(getActivity().getApplicationContext());
-//        Intent login = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
-//        login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        startActivity(login);
-//    }
+    private class ProfileAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return profileList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return profileList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View rowView = getActivity().getLayoutInflater().inflate(R.layout.row,null);
+            LinearLayout linearLayout = (LinearLayout) rowView.findViewById(R.id.rowLayout);
+            ImageView imageView = new ImageView(getActivity().getApplicationContext());
+            imageView.setImageResource(R.drawable.facebookicon);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+            imageView.setLayoutParams(layoutParams);
+            linearLayout.addView(imageView);
+            return linearLayout;
+        }
+    }
+
+    private View drawRow(View v, int id){
+
+
+    }
 }
--- app/src/main/java/com/jtruscott/biztag/fragments/RegisterFragment.java
+++ app/src/main/java/com/jtruscott/biztag/fragments/RegisterFragment.java
@@ -1,27 +1,103 @@
 package com.jtruscott.biztag.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.jtruscott.biztag.R;
+import com.jtruscott.biztag.library.DatabaseHandler;
+import com.jtruscott.biztag.library.UserFunctions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Josh on 2/12/2015.
  */
 public class RegisterFragment extends Fragment {
-    public RegisterFragment(){
+
+    Button btnRegister;
+    Button btnLinkToLogin;
+    EditText inputFullName;
+    EditText inputEmail;
+    EditText inputPassword;
+    TextView registerErrorMsg;
+
+    // JSON Response node names
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_UID = "uid";
+    private static String KEY_NAME = "name";
+    private static String KEY_EMAIL = "email";
+    private static String KEY_CREATED_AT = "created_at";
+
+    public RegisterFragment() {
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_register, container, false);
+        inputFullName = (EditText) v.findViewById(R.id.registerName);
+        inputEmail = (EditText) v.findViewById(R.id.registerEmail);
+        inputPassword = (EditText) v.findViewById(R.id.registerPassword);
+        btnRegister = (Button) v.findViewById(R.id.btnRegister);
+        btnLinkToLogin = (Button) v.findViewById(R.id.btnLinkToLoginScreen);
+        registerErrorMsg = (TextView) v.findViewById(R.id.register_error);
 
+        // Register Button Click event
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String name = inputFullName.getText().toString();
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                UserFunctions userFunction = new UserFunctions();
+                JSONObject json = userFunction.registerUser(name, email, password);
+
+                // check for login response
+                try {
+                    if (json.getString(KEY_SUCCESS) != null) {
+                        registerErrorMsg.setText("");
+                        String res = json.getString(KEY_SUCCESS);
+                        if (Integer.parseInt(res) == 1) {
+                            // user successfully registred
+                            // Store user details in SQLite Database
+                            DatabaseHandler db = new DatabaseHandler(getActivity().getApplicationContext());
+                            JSONObject json_user = json.getJSONObject("user");
+
+                            // Clear all previous data in database
+                            userFunction.logoutUser(getActivity().getApplicationContext());
+                            db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));
+                            // Launch Dashboard Screen
+                            FragmentTransaction ft = getFragmentManager().beginTransaction();
+                            ft.replace(R.id.fragment_content, new ProfileFragment());
+                            ft.commit();
+                        } else {
+                            // Error in registration
+                            registerErrorMsg.setText("Error occured in registration");
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Link to Login Screen
+        btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_content, new LoginFragment());
+                ft.commit();
+            }
+        });
         return v;
-
     }
 }
